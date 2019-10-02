0f63e92e41a384dfef6abfddc495917eab1ae346
yoomann000 <56059117+yoomann000@users.noreply.github.com>
2019-10-02 17:05:23 +0700

    Create ketik.sh

---
+++ ketik.sh
@@ -0,0 +1,26 @@
+pkg update && pkg upgrade
+pkg install python
+pkg install python2
+pkg install python3
+pkg install git
+pkg install php
+pkg install nano
+pkg install figlet
+pkg install cowsay
+pkg install toilet
+pkg install ruby
+pkg install lolcat
+pkg install neofetch
+pip install mechanize
+pip install requests
+pip install bs4
+pip install module
+pip2 install mechanize
+pip2 install requests
+pip2 install bs4
+pip2 install module
+gem install ruby
+gem install figlet
+gem install lolcat
+gem install cowsay
+echo "Jangan DiRecode Bgsd!:v "
